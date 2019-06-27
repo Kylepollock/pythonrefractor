23de66288aa3942c16bab8b98a7bb9d9da4656f4
Kyle Pollock <polloc21@gmail.com>
2019-06-26 17:11:53 -0700

    add python buid

---
+++ build.py
@@ -0,0 +1,34 @@
+def main():
+	#index
+	top_template = open('templates/top.html').read()
+	body = open('content/index.html').read()
+	bottom_template = open('templates/bottom.html').read()
+	index_html = top_template + body + bottom_template 
+	open('docs/index.html', 'w+').write(index_html)
+
+	#testimonial 
+	content = open('content/testimonials.html').read()
+	testimonials = top_template + content + bottom_template
+	open('docs/testimonials.html', 'w+').write(testimonials) 
+
+
+
+	#blog 
+	page = open('content/blog.html').read()
+	blog = top_template + page + bottom_template
+	open('docs/blog.html', 'w+').write(blog) 
+
+
+	#blog
+	page = open('content/blog.html').read()
+	blog = top_template + page + bottom_template
+	open('docs/blog.html', 'w+').write(blog) 
+
+	#blog
+	#testimonial 
+	page = open('content/blog.html').read()
+	blog = top_template + page + bottom_template
+	open('docs/blog.html', 'w+').write(blog) 
+
+if __name__ == "__main__":
+    main()
