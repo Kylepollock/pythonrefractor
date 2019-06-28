6b67a912cdb043435b0fbb719b631f45be441a50
Kyle Pollock <polloc21@gmail.com>
2019-06-27 18:53:49 -0700

    new template changes

--- build.py
+++ build.py
@@ -5,24 +5,25 @@ def main():
 	bottom = ('templates/bottom.html')
 	blog = ('content/blog.html')
 	testimonials = ('content/testimonials.html')
+	index = ('content/index.html')
 
-#combie top.html with bottom.htm ito base.html with {{content}}
+#combine top.html with bottom.htm ito base.html with {{content}}
 	
 	top_template = open(top).read()
 	bottom_template = open(bottom).read()
 	base_template = top_template + "{{content}}" + bottom_template
-	open('template/base.html', 'w+').write(base_template)
+	# open('template/base.html', 'w+').write(base_template)
 
 
 if __name__ == "__main__":
   main()
 
-template = open('template/base.html').read() 
+# template = open('template/base.html').read() 
 
 
+def listdic():
 
-
-'''pages = [ {
+	pages = [ {
         "filename": "content/index.html",
         "output": "docs/index.html",
         "title": "Allsprout",
@@ -37,33 +38,45 @@ template = open('template/base.html').read()
 		"output": "docs/blog.html",
 		"title": "Allsprout Blog",
 		}
-		]'''
+		]
 
 
-	
+	for page in pages:
+		# template = open('template/base.html').read()
+		filename = open(page['filename']).read()
+		# combined_file = template.replace("{{content}}",filename)
+		# open(page['output'],'w+').write(combined_file)
 
-#testimonial 
-content = open('content/testimonials.html').read()
-testimonials = top_template + content + bottom_template
-open('docs/testimonials.html', 'w+').write(testimonials) 
+listdic()
+
+#create and combine index html 
+# index_content = open(index).read()
+# finished_index_page = template.replace("{{content}}", index_content)
+# open('docs.html', 'w+').write(finished_index_page)
+
+
+##testimonial 
+# content = open('content/testimonials.html').read()
+# testimonials = top_template + content + bottom_template
+# open('docs/testimonials.html', 'w+').write(testimonials) 
 
 
 
-#blog 
-page = open('content/blog.html').read()
-blog = top_template + page + bottom_template
-open('docs/blog.html', 'w+').write(blog) 
+# #blog 
+# page = open('content/blog.html').read()
+# blog = top_template + page + bottom_template
+# open('docs/blog.html', 'w+').write(blog) 
 
-#blog
-page = open('content/blog.html').read()
-blog = top_template + page + bottom_template
-open('docs/blog.html', 'w+').write(blog) 
+# #blog
+# page = open('content/blog.html').read()
+# blog = top_template + page + bottom_template
+# open('docs/blog.html', 'w+').write(blog) 
 
-#blog
-#testimonial 
-page = open('content/blog.html').read()
-blog = top_template + page + bottom_template
-open('docs/blog.html', 'w+').write(blog) 
+# #blog
+# #testimonial 
+# page = open('content/blog.html').read()
+# blog = top_template + page + bottom_template
+# open('docs/blog.html', 'w+').write(blog) 
 
 
 
